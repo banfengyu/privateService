700f8d756b6ff5577d07c8466528e30d03641e11
[qianshengzhi] <[2236812156@qq.com]>
2020-09-23 21:19:36 +0800

    新增mybatisMaven模块

---
+++ MyBatisMavenDemo/src/main/java/com/itqf/util/Utils.java
@@ -0,0 +1,19 @@
+package com.itqf.util;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 小熊猫 2020/9/23 15:01
+ */
+public class Utils {
+    public static void main(String[] args) {
+
+        try {
+            InputStream inputStream = Resource.get
+        }catch (IOException e){
+
+        }
+    }
+}
